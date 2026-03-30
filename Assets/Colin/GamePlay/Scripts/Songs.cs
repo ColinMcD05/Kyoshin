076249849b2bf0781a540ab30f17c6abe92f382b
Colin McDaniel <colinmcd05@gmail.com>
2026-03-30 16:09:48 -0400

    Stuff

---
+++ Assets/Colin/GamePlay/Scripts/Songs.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Songs : MonoBehaviour
+{
+    public class SongData
+    {
+        public string name;
+        public int levelIndex;
+        public int bpm;
+
+    }
+}
