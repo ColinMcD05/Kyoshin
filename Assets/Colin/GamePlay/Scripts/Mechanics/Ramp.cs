40e43a32fa468e9cc0b63b10d472600426ab4c0e
ColinMcD05 <colinmcd05@gmail.com>
2026-04-20 23:38:42 -0400

    Update Ramp.cs

--- Assets/Colin/GamePlay/Scripts/Mechanics/Ramp.cs
+++ Assets/Colin/GamePlay/Scripts/Mechanics/Ramp.cs
@@ -2,13 +2,21 @@ using UnityEngine;
 
 public class Ramp : MonoBehaviour
 {
+    MoveBackwards moveBackwards;
     public float addedForce;
+
+    void Start()
+    {
+        moveBackwards = GameObject.Find("Level").GetComponent<MoveBackwards>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             Vector3 forceAdded = new Vector3(0, addedForce, 0);
             other.GetComponent<Rigidbody>().AddForce(forceAdded, ForceMode.Impulse);
+            moveBackwards.forwardSpeed = moveBackwards.maxSpeed;
         }
     }
 }
